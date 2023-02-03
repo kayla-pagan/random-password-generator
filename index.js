3dbde9f24e7420874ef3d328538687d8d5449194
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-02-03 12:56:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ function randomCharacters() {
     return password
 }
 
+var copy = document.querySelectorAll(".copy"); 
+
+for (const copied of copy) { 
+  copied.onclick = function() { 
+    document.execCommand("copy"); 
+  };  
+  copied.addEventListener("copy", function(event) { 
+    event.preventDefault(); 
+    if (event.clipboardData) { 
+      event.clipboardData.setData("text/plain", copied.textContent);
+      console.log(event.clipboardData.getData("text"))
+    };
+  });
+};
+
 
 
 
