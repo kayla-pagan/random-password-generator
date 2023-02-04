8e084c1d7187793e9cbe8cfe19b5c0f058fe1605
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-02-04 12:53:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOneEl = document.getElementById("password-one")
-let passwordTwoEl = document.getElementById("password-two")
+const passwordOneEl = document.getElementById("password-one")
+const passwordTwoEl = document.getElementById("password-two")
 
 function generatePass() {
     randomCharacters()
@@ -18,6 +18,11 @@ function randomCharacters() {
     }
     return password
 }
+
+const passBtn = document.getElementById("pass-btn")
+passBtn.addEventListener("click", function() {
+  generatePass()
+})
 
 var copy = document.querySelectorAll(".copy"); 
 
